9c2f8491885037f85e8622aa57c21b6808ae5072
Jarek Chmurak <smilech78@gmail.com>
2022-11-30 18:15:26 +0100

    Some functions created

--- sandbox/src/main/java/firstchapter/MyFirstProgram.java
+++ sandbox/src/main/java/firstchapter/MyFirstProgram.java
@@ -2,6 +2,26 @@ package firstchapter;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+
+    double len = 3.5;
+    System.out.println(area(len));
+
+    double x = 12;
+    double y = 4.9;
+    System.out.println(areaRectangle(x,y));
+  }
+
+  public static void hello(String somebody){
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l){
+    return l * l;
+  }
+
+  public static double areaRectangle(double x, double y){
+    return x * y;
   }
 }
