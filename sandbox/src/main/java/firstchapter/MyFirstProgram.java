cbb132f15d319e996a67ad5a23b7f494527b0975
Jarek Chmurak <smilech78@gmail.com>
2022-12-04 20:31:33 +0100

    Exercise 2

--- sandbox/src/main/java/firstchapter/MyFirstProgram.java
+++ sandbox/src/main/java/firstchapter/MyFirstProgram.java
@@ -1,20 +1,22 @@
 package firstchapter;
 
 public class MyFirstProgram {
-  public static void main(String[] args) {
-    hello("world");
-    hello("user");
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
 
-    Square s = new Square(5);
-    System.out.println(s.area());
+        Square s = new Square(5);
+        System.out.println(s.area());
 
-    Rectangle r = new Rectangle(5, 2);
-    System.out.println(r.areaRectangle());
-  }
+        Rectangle r = new Rectangle(5, 2);
+        System.out.println(r.areaRectangle());
 
-  public static void hello(String somebody){
-    System.out.println("Hello, " + somebody + "!");
-  }
+        Point p1 = new Point(5, -3);
+        Point p2 = new Point(0, 0);
+        System.out.println(Point.distance(p1,p2));
+    }
 
-
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
 }
