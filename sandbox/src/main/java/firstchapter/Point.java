34008f649f5dd45e63f5f85f3cb3d065ed33a406
Jarek Chmurak <smilech78@gmail.com>
2022-12-05 20:51:33 +0100

    First test created

--- sandbox/src/main/java/firstchapter/Point.java
+++ sandbox/src/main/java/firstchapter/Point.java
@@ -3,7 +3,6 @@ package firstchapter;
 import static java.lang.Math.pow;
 
 public class Point {
-
         public double x;
         public double y;
 
@@ -15,7 +14,6 @@ public class Point {
     public static double distance(Point p1, Point p2) {
         double a = p1.x - p2.x;
         double b = p1.y - p2.y;
-
         return Math.sqrt(pow(a,2) + pow(b,2));
     }
 }
