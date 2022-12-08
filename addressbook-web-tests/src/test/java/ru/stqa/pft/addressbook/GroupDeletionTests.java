e33523f47cd3b14c95dc6b91da5871acd76d13f5
Jarek Chmurak <smilech78@gmail.com>
2022-12-08 20:57:15 +0100

    TestBase Creation
    Commit contains creation of TestBase and GroupDeletionTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.*;
+
+public class GroupDeletionTests extends TestBase {
+
+  @Test
+  public void testGroupDeletion() {
+    goToGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+}
