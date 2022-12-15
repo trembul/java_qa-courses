65cea72fd32c41edbe770ee98202f7e0f685bf3e
Jarek Chmurak <smilech78@gmail.com>
2022-12-15 21:48:55 +0100

    Equation simplification

--- sandbox/src/test/java/firstchapter/EquationTests.java
+++ sandbox/src/test/java/firstchapter/EquationTests.java
@@ -20,4 +20,19 @@ public class EquationTests {
         Equation e = new Equation(1, 5, 6);
         Assert.assertEquals(e.rootNumber(), 2);
     }
+
+    public void testLinear(){
+        Equation e = new Equation(0, 1, 1);
+        Assert.assertEquals(e.rootNumber(), 1);
+    }
+
+    public void testConstant(){
+        Equation e = new Equation(0, 0, 1);
+        Assert.assertEquals(e.rootNumber(), 0);
+    }
+
+    public void testZero(){
+        Equation e = new Equation(0, 0, 0);
+        Assert.assertEquals(e.rootNumber(), -1);
+    }
 }
