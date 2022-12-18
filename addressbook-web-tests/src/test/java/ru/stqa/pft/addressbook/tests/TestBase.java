ec4e9557f6a75f3a1056465c1c405b7b82501241
Jarek Chmurak <smilech78@gmail.com>
2022-12-18 21:56:14 +0100

    Tests in Firefox

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -7,7 +7,7 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(Browser.IE);
+    protected final ApplicationManager app = new ApplicationManager(Browser.FIREFOX);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
