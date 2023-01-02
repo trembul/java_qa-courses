899a4bea2959bad0da3a460ad8cc96548af348b2
Jarek Chmurak <smilech78@gmail.com>
2023-01-02 20:47:17 +0100

    Comparing collections

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public class GroupData {
     private final String name;
     private final String header;
@@ -23,4 +25,23 @@ public class GroupData {
         return footer;
     }
 
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        final GroupData groupData = (GroupData) o;
+        return Objects.equals(this.name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.name);
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
